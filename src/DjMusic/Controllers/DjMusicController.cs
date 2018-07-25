b80fe694a7889392cc131bdadbca1382d096f87c
Chelsea Mofiz <chelseamofiz@gmail.com>
2018-07-24 21:31:44 -0400

    Added detail and action method

---
+++ src/DjMusic/Controllers/DjMusicController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DjMusic.Controllers
+{
+    public class DjMusicController : Controller
+    {
+        public string Detail()
+        {
+            return "Hello from the Dj Music controller!";
+        }
+    
+    }
+}
