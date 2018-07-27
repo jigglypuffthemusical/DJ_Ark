d65866b7c94bff5b340583ac4222a9914e9be588
Chelsea Mofiz <chelseamofiz@gmail.com>
2018-07-27 04:55:39 -0400

    Final push finished Backend Project.

---
+++ WebApplication1/WebApplication1/App_Start/RouteConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+/*Made sure the routeConfig routed to Customers where all my SQL server database was at instead of the default home.*/
+namespace WebApplication1
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Customers", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
